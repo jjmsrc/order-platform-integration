d494e7d468c0ebb0a65473b6775a00cc4d3945db
jjmsrc <mnvyiions@gmail.com>
2026-02-04 00:28:46 +0900

    refactor: import 구문 정리

--- src/main/java/com/jm/orderplatform/repository/OrderRepository.java
+++ src/main/java/com/jm/orderplatform/repository/OrderRepository.java
@@ -1,17 +1,13 @@
 package com.jm.orderplatform.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.jm.orderplatform.entity.OrderEntity;
 import com.jm.orderplatform.entity.OrderPk;
-
-import jakarta.persistence.LockModeType;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, OrderPk> {
 
